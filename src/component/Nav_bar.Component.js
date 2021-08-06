b3d069d302c7cd6dc8a8e234fb3c3ef9327ac78d
AAkaliT22 <amrt200000.gmail.com>
2021-08-06 10:49:31 +0200

    set style changes

--- src/component/Nav_bar.Component.js
+++ src/component/Nav_bar.Component.js
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWaveSquare } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function NavBar(props){
     return( 
         <header className = " Nav-bar" >
-            <h1>Waves</h1>
+            <FontAwesomeIcon size="1x" icon ={faWaveSquare} />
             <button onClick ={()=>{
                 console.log("clicked")
             }}>Library</button>
